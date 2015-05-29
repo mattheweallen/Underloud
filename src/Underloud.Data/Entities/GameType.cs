d64a91434aabf22f10eb1a2174955aeeb78b2e5a
mattheweallen <mattheweallen@gmail.com>
2015-05-29 06:33:09 -0500

    added Player, Game Entity classes and Entity Framework Ref

---
+++ src/Underloud.Data/Entities/GameType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Underloud.Data.Entities
+{
+    public class GameType
+    {
+    }
+}
---
+++ src/Underloud.Data/Entities/PlayerRank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Underloud.Data.Entities
+{
+    public class PlayerRank
+    {
+        public int Id { get; set; }
+        public int Rank { get; set; }
+        public virtual Player Player { get; set; }
+        public virtual Game Game { get; set; }
+    }
+}
